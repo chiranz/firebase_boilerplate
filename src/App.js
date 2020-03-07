c31ae6c3e2dbb9205dc2e684149f5406a57db74d
chiranz <chiranpoudyal@gmail.com>
2020-03-07 17:15:33 +0530

    Register page setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./app.style.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Login from "./components/Login";
 import { auth } from "./firebase/firebase.utils";
+import Register from "./components/Register";
 
 function App() {
   const [email, setEmail] = useState(null);
@@ -22,13 +23,17 @@ function App() {
           {email ? (
             <h6 onClick={() => auth.signOut()}>Logout</h6>
           ) : (
-            <Link to="/login">Login</Link>
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
           )}
         </div>
       </div>
       <Switch>
         <Route exact path="/" render={() => <h1>Homepage</h1>} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </Router>
   );
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,16 +3,23 @@ import { TextField, Button } from "@material-ui/core";
 import { SignInWithGoogle } from "../firebase/firebase.utils";
 import { AiOutlineGoogle } from "react-icons/ai";
 
-export default function Login() {
+export default function Register() {
   return (
     <div>
+      <TextField
+        id="username"
+        type="text"
+        name="username"
+        label="Username"
+        variant="outlined"
+      />
+      <br />
       <TextField
         id="email"
         type="text"
         name="password"
         label="Email"
         variant="outlined"
-        style={{ flex: 1 }}
       />
       <br />
 
@@ -22,14 +29,22 @@ export default function Login() {
         name="password"
         label="Password"
         variant="outlined"
-        style={{ flex: 1 }}
       />
       <br />
 
-      <Button variant="contained">Sign In</Button>
+      <TextField
+        id="confirm_password"
+        type="password"
+        name="confirm_password"
+        label="Confirm Password"
+        variant="outlined"
+      />
+      <br />
+
+      <Button variant="contained">Sign Up</Button>
       <Button variant="contained" color="primary" onClick={SignInWithGoogle}>
         <AiOutlineGoogle style={{ color: "yellow", marginRight: ".5rem" }} />{" "}
-        Sign In
+        Register with
       </Button>
     </div>
   );
